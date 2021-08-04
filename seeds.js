4ccd929874154554a812d72fedd829658f8a2ca1
mayanhavoc <mayanhavoc@gmail.com>
2021-08-03 20:43:36 -0700

    seedProducts

--- seeds.js
+++ seeds.js
@@ -10,3 +10,50 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
     console.log(err);
 })
 
+// const p = new Product({
+//     name: 'Ruby grapefruit',
+//     price: 1.99,
+//     category: 'fruit'
+// })
+// p.save().then(p => {
+//     console.log(p)
+// })
+// .catch(e => {
+//     console.log(e)
+// })
+
+const seedProducts = [
+      {
+          name: 'Fairy eggplant',
+          price: 1.00,
+          category: 'vegetables '
+      },
+      {
+          name: 'Organic goddess melon',
+          price: 4.99,
+          category: 'fruit'
+      },
+      {
+          name: 'Organic mini seedless watermelon',
+          price: 3.99,
+          category: 'fruit'
+      },
+      {
+          name: 'Organic celery',
+          price: 1.50,
+          category: 'vegetables'
+      },
+      {
+          name: 'Chocolate whole milk',
+          price: 2.69,
+          category: 'diary'
+      },
+]
+
+Product.insertMany(seedProducts)
+.then(res => {
+    console.log(res)
+})
+.catch(e => {
+    console.log(e)
+})
